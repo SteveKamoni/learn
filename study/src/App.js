6ac6305f3ae68461aea5b98bbfa73a9ee8dd9cb5
SteveKamoni <stevekamoni4@gmail.com>
2024-11-07 11:45:26 +0300

    events

--- study/src/App.js
+++ study/src/App.js
@@ -22,11 +22,64 @@ function App() {
     return <button onClick={onSmash}>{prop}</button>;
   };
 
+  const ButtonProp = ({ onClick, children }) => {
+    return <button onClick={onClick}>{children}</button>;
+  };
+
+  function PlayButton({ movieName }) {
+    function handleClickPlay() {
+      alert(`Playing ${movieName}`);
+    }
+
+    return <ButtonProp onClick={handleClickPlay}>Play</ButtonProp>;
+  }
+
+  function UploadButton() {
+    function handleUpload() {
+      alert("Uploading...");
+    }
+
+    return <ButtonProp onClick={handleUpload}>Upload</ButtonProp>;
+  }
+
+  function ToolbarButton({ onClick, children }) {
+    return (
+      <button
+        onClick={(e) => {
+          e.stopPropagation();
+          onClick();
+        }}
+      >
+        {children}
+      </button>
+    );
+  }
+  function Toolbar() {
+    return (
+      <div
+        className="Toolbar"
+        onClick={() => {
+          alert("You clicked on the toolbar!");
+        }}
+      >
+        <ToolbarButton onClick={() => alert("Playing!")}>
+          Play Movie
+        </ToolbarButton>
+        <ToolbarButton onClick={() => alert("Uploading!")}>
+          Upload Image
+        </ToolbarButton>
+      </div>
+    );
+  }
+
   return (
     <>
       <Greeting name={"Steve"} />
       <Clicked />
       <Button onSmash={() => alert("Naming Event hanler props")}></Button>
+      <PlayButton movieName={"John Wick Chapter 4"} />
+      <UploadButton />
+      <Toolbar />
     </>
   );
 }
